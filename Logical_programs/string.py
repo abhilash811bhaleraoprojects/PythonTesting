a926643ee797669b6a6dab14e434a7c0ead1c307
Abhilash Bhalerao <bhalerao.abhilash9@gmail.com>
2025-08-16 19:44:08 +0530

    codechangesdone

--- Logical_programs/string.py
+++
@@ -1,9 +0,0 @@
-#reversre a string without using slicing
-def reverse_string(s):
-    rev = ""
-    for char in s:
-        rev = char + rev
-    return rev
-
-# Test
-print(reverse_string("Selenium"))  # muineleS
---
+++ Python_logical_programs/number_swap.py
@@ -0,0 +1,19 @@
+
+#wap how to swap two numbers
+#approach 1
+num1 = 10
+num2 = 20
+print("value of num1 before swapping", num1)
+print("value of num2 before swapping", num2)
+
+#temp = num1
+#num1 = num2
+#num2 = temp
+
+#approach 2
+num1, num2 = num2 , num1
+
+print("value of num1 after swapping", num1)
+print("value of num2 after swapping", num2)
+
+
