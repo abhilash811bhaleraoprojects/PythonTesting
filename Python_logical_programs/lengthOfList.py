afafe1e9c23f36970c559784c9fae48fa623612e
Abhilash Bhalerao <bhalerao.abhilash9@gmail.com>
2025-08-17 13:01:13 +0530

    codechangesdone

---
+++ Python_logical_programs/lengthOfList.py
@@ -0,0 +1,13 @@
+#Approach 1
+myList = [1, 3, 5, 9, 12, 43, 54]
+
+count =0
+
+for i in myList:
+    count = count+1
+
+print("lenght of list is ", count)
+
+#Approach 2(Using inbuilt functon)
+
+print("lenght of list is ", len(myList))
